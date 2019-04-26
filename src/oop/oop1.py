74f5d037e46079dfc8c08a561d0f25e460a0bf76
Modesto Tamayo <modesto.t.tamayo@gmail.com>
2019-04-26 10:59:48 -0500

    Completed the first oop file

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,29 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+# Base class
+class Vehicle:
+  pass
+
+# Class is subclass of Vehicle
+class GroundVehicle(Vehicle):
+  pass
+
+# Class is subclass of Vehicle
+class FlightVehicle(Vehicle):
+  pass
+
+# Classes are subclasses of GroundVehicle
+class Car(GroundVehicle):
+  pass
+
+class Motorcycle(GroundVehicle):
+  pass
+
+# Classes are subclasses of FlightVehicle
+class Airplane(FlightVehicle):
+  pass
+
+class Starship(FlightVehicle):
+  pass
